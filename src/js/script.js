e6da50ac6eaa42c957796ee6718d089babd07a3b
Otávio <otaviogrocha@yahoo.com.br>
2022-10-25 18:06:21 -0300

    commit files

---
+++ src/js/script.js
@@ -0,0 +1,38 @@
+const inputs = document.querySelectorAll('.input')
+const button = document.querySelector('.login_button');
+const video = document.querySelector('.back_video')
+
+
+const focusAnimation = (event) => { 
+    const span = event.target.previousElementSibling;
+    span.classList.add('span-active');
+}
+
+
+const focusAnimationOut = (event) => { 
+    if (event.target.value == "") {
+        const span = event.target.previousElementSibling;
+        span.classList.remove('span-active');        
+    }    
+}
+
+
+const charVerify = () =>{
+    const [username, password] = inputs;
+    if (username.value && password.value.length >= 1){
+        button.removeAttribute('disabled')
+    }else{
+        button.setAttribute('disabled', '')
+    }
+
+}
+
+const setVolume = ()=>{
+    video.volume = 0.1;
+}
+
+inputs.forEach((input) => input.addEventListener('focus', focusAnimation));
+inputs.forEach((input) => input.addEventListener('focusout', focusAnimationOut));
+inputs.forEach((input) => input.addEventListener('input', charVerify));
+
+setVolume();
