aac6f05b210ec7ca3f00dd535d662f298a79521e
Otávio <otaviogrocha@yahoo.com.br>
2022-10-27 15:41:11 -0300

    fix video autoplay + add mute button

--- src/js/script.js
+++ src/js/script.js
@@ -1,35 +1,36 @@
 const inputs = document.querySelectorAll('.input')
 const button = document.querySelector('.login_button');
 const video = document.querySelector('.back_video')
+const mute = document.querySelector('.mute-button')
+const img = document.querySelector('.img-button');
 
 
-const focusAnimation = (event) => { 
+const focusAnimation = (event) => {
     const span = event.target.previousElementSibling;
     span.classList.add('span-active');
 }
 
 
-const focusAnimationOut = (event) => { 
+const focusAnimationOut = (event) => {
     if (event.target.value == "") {
         const span = event.target.previousElementSibling;
-        span.classList.remove('span-active');        
-    }    
+        span.classList.remove('span-active');
+    }
 }
 
-
-const charVerify = () =>{
+const charVerify = () => {
     const [username, password] = inputs;
-    if (username.value && password.value.length >= 1){
+    if (username.value && password.value.length >= 1) {
         button.removeAttribute('disabled')
-    }else{
+    } else {
         button.setAttribute('disabled', '')
     }
 
 }
 
-const videoStart = ()=>{
-    video.volume = 0.2;
-    video.removeAttribute(muted);
+const videoStart = () => {
+    // video.muted = false; 
+    video.volume = 0.2;       
     video.play();
 }
 
@@ -37,4 +38,25 @@ inputs.forEach((input) => input.addEventListener('focus', focusAnimation));
 inputs.forEach((input) => input.addEventListener('focusout', focusAnimationOut));
 inputs.forEach((input) => input.addEventListener('input', charVerify));
 
-videoStart();
+mute.addEventListener('click', ()=>{
+    
+    if (img.classList.contains('active')) {
+        img.classList.remove('active')
+        console.log(img)
+        video.muted = false; 
+        video.volume = 0.2;  
+        mute.firstElementChild.src = './src/img/soundup.png'
+    }
+    else{
+        video.muted = true;        
+        img.classList.add('active')
+        mute.firstElementChild.src = './src/img/mute.png'
+    }
+
+    // if (img.classList.contains('active')){
+    //     video.muted = true;
+    //     mute.firstElementChild.src = './src/img/mute2.png'
+    // } 
+});
+
+window.addEventListener('load', videoStart);
